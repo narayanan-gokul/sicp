5ceea2368d03c7338d72b710826152bb9cee077c
Gokul Narayanan <gokul.narayanan@pm.me>
2024-10-30 16:20:27 +0530

    Nested mappings

--- scripts/nested-mappings.scm
+++ scripts/nested-mappings.scm
@@ -39,8 +39,48 @@
        (filter even-sum?
 	       (pairs n))))
 
-(display (even-sum-pairs 5))
+(define (remove x s)
+  (filter (lambda (y) (not (= x y))) s))
+
+(define (permutations s)
+  (if (null? s)
+    (list #nil)
+    (flatmap
+      (lambda (x) (map (lambda (p) (cons x p)) (permutations (remove x s))))
+      s)))
+
+(define (ordered-triples n)
+  (flatmap
+    (lambda
+      (i)
+      (flatmap
+	(lambda
+	  (j)
+	  (map
+	    (lambda
+	      (k)
+	      (list k j i))
+	    (enumerate-interval 1 (- j 1))))
+	(enumerate-interval 1 (- i 1))))
+    (enumerate-interval 1 n)))
+
+(define (sum-triples sum n)
+  (filter
+    (lambda (triple)
+      (= sum (+ (car triple) (cadr triple) (caddr triple))))
+    (ordered-triples n)))
+
+(display (sum-triples 9 5))
 (newline)
+
+; (display (ordered-triples 5))
+; (newline)
+
+; (display (permutations (list 1 2 3)))
+; (newline)
+
+; (display (even-sum-pairs 5))
+; (newline)
 ; 
 ; (display (pairs 5))
 ; (newline)
